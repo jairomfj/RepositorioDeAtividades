9306b60fbe753f04d99c301192d712f2264dff1d
Jairo Mendes <jairomfj@gmail.com>
2016-05-06 19:19:11 -0300

    Melhoria na tela de login

--- src/main/java/br/com/repositoriodeatividades/controllers/AbstractController.java
+++ src/main/java/br/com/repositoriodeatividades/controllers/AbstractController.java
@@ -3,12 +3,10 @@ package br.com.repositoriodeatividades.controllers;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 
-/**
- * Created by jairomendes on 4/30/16.
- */
 abstract class AbstractController {
 
     protected User getCurrentUser() {
         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
+
 }
