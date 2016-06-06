56ceac0d7024580b68d63eeef2f6385b6523aedc
Jairo Mendes <jairomfj@gmail.com>
2016-06-06 12:21:10 -0300

    Aumento do upload de arquivo para 5mb e refactoring no ExerciseEnumeration

--- src/main/java/br/com/repositoriodeatividades/usecases/exercise/utils/enums/ExerciseEnumeration.java
+++ src/main/java/br/com/repositoriodeatividades/usecases/exercise/utils/enums/ExerciseEnumeration.java
@@ -2,29 +2,24 @@ package br.com.repositoriodeatividades.usecases.exercise.utils.enums;
 
 public enum ExerciseEnumeration {
 
-    BULLET("^\\u2022", "•"),
-    NUMBER_BAR("^\\d\\)","^\\d"),
-    NUMBER_DOT("^\\d\\.", "^\\d"),
-    NUMBER_HYPHEN("^\\d-", "^\\d"),
-    CHAR_BAR("^\\A[^\\W\\d_]{1}\\)", "^\\A[^\\W\\d_]{1}"),
-    CHAR_DOT("^\\A[^\\W\\d_]{1}\\.", "^\\A[^\\W\\d_]{1}"),
-    CHAR_HYPHEN("^\\A[^\\W\\d_]{1}-", "\\A[^\\W\\d_]{1}"),
-    PARENTHESIS("\\(\\)", "\\(\\)"),
-    NONE("ˆ\\w", "ˆ\\w");
+    BULLET("^\\u2022"),
+    NUMBER_BAR("^\\d*\\)"),
+    NUMBER_DOT("^\\d*\\."),
+    NUMBER_HYPHEN("^\\d*-"),
+    CHAR_BAR("^\\A[^\\W\\d_]{1}\\)"),
+    CHAR_DOT("^\\A[^\\W\\d_]{1}\\."),
+    CHAR_HYPHEN("^\\A[^\\W\\d_]{1}-"),
+    PARENTHESIS("\\(\\)"),
+    NONE("ˆ\\w");
 
     private String fullEnumeration;
-    private String shortEnumeration;
 
-    ExerciseEnumeration (String fullEnumeration, String shortEnumeration) {
+    ExerciseEnumeration (String fullEnumeration) {
         this.fullEnumeration = fullEnumeration;
-        this.shortEnumeration = shortEnumeration;
     }
 
     public String getFullEnumeration() {
         return fullEnumeration;
     }
 
-    public String getShortEnumeration() {
-        return shortEnumeration;
-    }
 }
