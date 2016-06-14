a75bf7829d25be6dc2199a2b7f430c126cc66650
Jairo Mendes <jairomfj@gmail.com>
2016-06-13 22:48:09 -0300

    Ajustes nas configs

--- src/main/java/br/com/repositoriodeatividades/Application.java
+++ src/main/java/br/com/repositoriodeatividades/Application.java
@@ -10,11 +10,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
-@Profile("production")
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
+@PropertySource(value = { "file:/home/ec2-user/repositorio/application-production.properties" })
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
